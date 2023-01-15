ec4e288e3fbd0840999e8e649ea4f56d9fd1b79a
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2023-01-15 17:33:17 +0900

    [Pratice] navermap 의존성 및 띄우기

---
+++ app/src/main/java/llama/test/jetpack_dagger_plz/main.kt
@@ -0,0 +1,12 @@
+package llama.test.jetpack_dagger_plz
+
+fun main(){
+    print(wow())
+}
+
+fun hello() : String{
+    val hello = "String"
+    return hello
+}
+
+fun wow () : String = "hello"
--- app/src/main/java/llama/test/jetpack_dagger_plz/presentation/MainActivity.kt
+++ app/src/main/java/llama/test/jetpack_dagger_plz/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package llama.test.jetpack_dagger_plz.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -12,14 +13,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.compose.CameraPositionState
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
 import dagger.hilt.android.AndroidEntryPoint
 import llama.test.jetpack_dagger_plz.presentation.screens.HomeScreen
 import llama.test.jetpack_dagger_plz.presentation.screens.viewmodel.TestViewModel
 import llama.test.jetpack_dagger_plz.ui.theme.JETPACK_DAGGER_PLZTheme
 import llama.test.jetpack_dagger_plz.utils.Common.TAG
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.compose.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -33,7 +44,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     MyApp{
-                        PLZ()
+                        Navermap()
+//                        PLZ()
                     }
                 }
             }
@@ -69,6 +81,34 @@ fun PLZ(viewModel: TestViewModel = hiltViewModel()){
 
         }
 
+    }
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun Navermap(){
+    val seoul = LatLng(37.532600, 127.024612)
+    val context = LocalContext.current
+
+    val camerapositionState:CameraPositionState = rememberCameraPositionState{
+        position = CameraPosition(seoul , 11.0)
+    }
+
+    Box(modifier = Modifier.fillMaxSize()){
+       NaverMap(
+           cameraPositionState = camerapositionState
+       ){
+           Marker(
+               state = MarkerState(position = seoul),
+               captionText = Offset.Infinite.toString(),
+               height = 36.dp,
+               width = 36.dp,
+               onClick = {
+                   Toast.makeText(context, it.position.longitude.toString(), Toast.LENGTH_SHORT).show()
+                   true
+               }
+           )
+       }
     }
 }
 
